225149ae808fc45342b0b44a21822c5e6877f15a
G00380141 <55436456+G00380141@users.noreply.github.com>
2020-04-27 13:48:06 +0100

    Exercise 1

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1{
+
+    public static void main(String[] args){
+
+        String name = "Mary";
+        int age = 20;
+        double weight = 65.48;
+        String color = "red";
+
+        System.out.println(name+ " is " + age + " years old.");
+        System.out.println("She weighs " + weight + " kilograms.");
+        System.out.println("Her favouriter colour is " + color + ".");
+
+    }
+}
